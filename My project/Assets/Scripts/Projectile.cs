b6a8ce56e9c3e9a4f2063214e8225b056d775718
BenKTAMU <145497372+BenKTAMU@users.noreply.github.com>
2025-03-10 15:02:28 -0500

    m1v1 does NOT equal m2v2

--- My project/Assets/Scripts/Projectile.cs
+++ My project/Assets/Scripts/Projectile.cs
@@ -1,29 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class Projectile : MonoBehaviour
 {
+    public float bounciness = 0.8f;
+    public float lifetime = 2f;
+    private Rigidbody2D rb;
 
-    
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    void OnEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Enemy")
-        {
-            Debug.Log("Projectile hit an enemy.");
-            Destroy(gameObject);
-        }
+        Vector2 normal = collision.contacts[0].normal;
+        Vector2 incomingVelocity = rb.velocity;
+        Vector2 reflectedVelocity = Vector2.Reflect(incomingVelocity, normal) * bounciness;
+        rb.velocity = reflectedVelocity;
     }
 }
---
+++ My project/Assets/Scripts/ThrowManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class ThrowManager : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public GameObject playerObj;
+    public LineRenderer lineRenderer;
+    public float powerMultiplier;
+    public LayerMask wallLayerMask;
+    private Vector3 dragStartPos;
+    private Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragStartPos = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 dragEndPos = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+            Vector3 force = (dragStartPos - dragEndPos) * powerMultiplier;
+            ShowTrajectory(playerObj.transform.position, force);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 dragEndPos = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+            Vector3 force = (dragStartPos - dragEndPos) * powerMultiplier;
+            FireProjectile(force);
+            lineRenderer.positionCount = 0; // Clear line
+        }
+    }
+
+    void ShowTrajectory(Vector3 startPos, Vector3 force)
+    {
+        lineRenderer.positionCount = 500; // Adjust for trajectory detail
+        Vector3 velocity = force;
+        float timestep = 0.1f;
+        Vector3 currentPos = startPos;
+        int bounceLimit = 3; // Set the maximum number of bounces
+        int bounceCount = 0;
+
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            lineRenderer.SetPosition(i, currentPos);
+
+            if (bounceCount >= bounceLimit)
+            {
+                currentPos += velocity * timestep;
+                continue;
+            }
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(currentPos, velocity, velocity.magnitude * timestep);
+            if (hits.Length > 0)
+            {
+                RaycastHit2D hit = hits[0];
+                Vector2 normal = hit.normal;
+                velocity = Vector2.Reflect(velocity, normal);
+                currentPos = hit.point + normal * 0.01f; // Move slightly away from the wall to avoid immediate re-collision
+                bounceCount++;
+            }
+            else
+            {
+                currentPos += velocity * timestep;
+            }
+        }
+    }
+    void FireProjectile(Vector3 force)
+    {
+        GameObject proj = Instantiate(projectilePrefab, playerObj.transform.position, Quaternion.identity);
+        Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = force;
+        }
+    }
+}
