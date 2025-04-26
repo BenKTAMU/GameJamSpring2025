eca474c4eadded499327e4e69e629f095baa8438
AveryNAlt <averynalthaus@gmail.com>
2025-04-25 22:59:10 -0500

    distracted guy works

--- My project/Assets/Scripts/EnemyAnimator.cs
+++ My project/Assets/Scripts/EnemyAnimator.cs
@@ -27,64 +27,66 @@ public class EnemyAnimator : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        parent_xrot = parent.transform.rotation.x;
+        parent_yrot = parent.transform.rotation.y;
+        parent_zrot = parent.transform.rotation.z;
+        gameObject.transform.rotation = Quaternion.Euler(-parent_xrot, - parent_yrot, - parent_zrot);
         if (sillyNumber == 1)
         {
-            parent_xrot = parent.transform.rotation.x;
-            parent_yrot = parent.transform.rotation.y;
-            parent_zrot = parent.transform.rotation.z;
-            gameObject.transform.rotation = Quaternion.Euler(-parent_xrot, - parent_yrot, - parent_zrot);
-            animator.SetFloat("IdleX", 1);
+            animator.SetBool("isMoving", false);
+            animator.SetFloat("IdleX", -1);
             animator.SetFloat("IdleY", 0);
         }
         else
         {
-            parent_xrot = parent.transform.rotation.x;
-            parent_yrot = parent.transform.rotation.y;
-            parent_zrot = parent.transform.rotation.z;
-            gameObject.transform.rotation = Quaternion.Euler(-parent_xrot, - parent_yrot, - parent_zrot);
         
             xDirection = parent.transform.position.x;
             yDirection = parent.transform.position.y;
             velocityX = (xDirection - lastXDirection)/Time.deltaTime;
             velocityY = (yDirection-lastYDirection)/Time.deltaTime;
-            
-            if(Mathf.Abs(velocityX) > 0.01f || Mathf.Abs(velocityY) > 0.01f)
+            if(parent.GetComponent<EnemyPatrolManual>().isDistracted == true)
             {
-                
-                if(parent.GetComponent<EnemyPatrolManual>().isChasing == false)
-                {
-                    animator.SetBool("isMoving", true);
-                    velocityX = (xDirection - lastXDirection)/Time.deltaTime;
-                    velocityY = (yDirection-lastYDirection)/Time.deltaTime;
-                    animator.SetFloat("moveX", velocityX);
-                    animator.SetFloat("moveY", velocityY);
-                }
-                else if(velocityX != 0 && velocityY != 0 || lastVelocityX != 0 && lastVelocityY != 0)
-                {
-                    if ( parent.GetComponent<EnemyPatrolManual>().isChasing == true)
-                    {
-                        animator.SetBool("isMoving", true);
-                        velocityX = parent.GetComponent<EnemyPatrolManual>().aiPath.velocity.x;
-                        velocityY = parent.GetComponent<EnemyPatrolManual>().aiPath.velocity.y;
-                        animator.SetFloat("moveX", velocityX);
-                        animator.SetFloat("moveY", velocityY);
-                    }
-                    else
-                    {
-                        animator.SetBool("isMoving", false);
-                        animator.SetFloat("IdleX", lastVelocityX);
-                        animator.SetFloat("IdleY", lastVelocityY);
-                    }
-
-                }
+                sillyNumber = 1;
             }
             else
             {
-                animator.SetBool("isMoving", false);
-                animator.SetFloat("IdleX", lastVelocityX);
-                animator.SetFloat("IdleY", lastVelocityY);
+                sillyNumber = 0;
+                if(Mathf.Abs(velocityX) > 0.01f || Mathf.Abs(velocityY) > 0.01f)
+                {
+                    if(parent.GetComponent<EnemyPatrolManual>().isChasing == false)
+                    {
+                        animator.SetBool("isMoving", true);
+                        velocityX = (xDirection - lastXDirection)/Time.deltaTime;
+                        velocityY = (yDirection-lastYDirection)/Time.deltaTime;
+                        animator.SetFloat("moveX", velocityX);
+                        animator.SetFloat("moveY", velocityY);
+                    }
+                    else if(velocityX != 0 && velocityY != 0 || lastVelocityX != 0 && lastVelocityY != 0)
+                    {
+                        if ( parent.GetComponent<EnemyPatrolManual>().isChasing == true)
+                        {
+                            animator.SetBool("isMoving", true);
+                            velocityX = parent.GetComponent<EnemyPatrolManual>().aiPath.velocity.x;
+                            velocityY = parent.GetComponent<EnemyPatrolManual>().aiPath.velocity.y;
+                            animator.SetFloat("moveX", velocityX);
+                            animator.SetFloat("moveY", velocityY);
+                        }
+                        else
+                        {
+                            animator.SetBool("isMoving", false);
+                            animator.SetFloat("IdleX", lastVelocityX);
+                            animator.SetFloat("IdleY", lastVelocityY);
+                        }
+                    }
+                }
+                else
+                {
+                    animator.SetBool("isMoving", false);
+                    animator.SetFloat("IdleX", lastVelocityX);
+                    animator.SetFloat("IdleY", lastVelocityY);
+                } 
             }
-
+            
             lastXDirection = xDirection;
             lastYDirection = yDirection;
             lastVelocityX = velocityX;
